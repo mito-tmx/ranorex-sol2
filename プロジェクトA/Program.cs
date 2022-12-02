0831178a01b1a9bd7456e6210b7f42546adfdabf
mito-tmx <motomu@techmatrix.co.jp>
2022-12-02 21:02:50 +0900

    test

---
+++ プロジェクトA/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by Ranorex
+ * User: motomu
+ * Date: 2022/12/02
+ * Time: 21:01
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+
+using System;
+using System.Threading;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Reporting;
+using Ranorex.Core.Testing;
+
+namespace プロジェクトA
+{
+    class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            // Uncomment the following 2 lines if you want to automate Windows apps
+            // by starting the test executable directly
+            //if (Util.IsRestartRequiredForWinAppAccess)
+            //    return Util.RestartWithUiAccess();
+
+            Keyboard.AbortKey = System.Windows.Forms.Keys.Pause;
+            int error = 0;
+
+            try
+            {
+                error = TestSuiteRunner.Run(typeof(Program), Environment.CommandLine);
+            }
+            catch (Exception e)
+            {
+                Report.Error("Unexpected exception occurred: " + e.ToString());
+                error = -1;
+            }
+            return error;
+        }
+    }
+}
